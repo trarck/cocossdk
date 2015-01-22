3dcf8dc13472128d277318a4a0c40b2af58b7df1
houhai.duan <houhai.duan@happyelements.com>
2015-01-22 19:03:15 +0800

    simple test project

---
+++ tests/TestOpenSdk/src/com/opensdk/testopensdk/MainApp.java
@@ -0,0 +1,16 @@
+package com.opensdk.testopensdk;
+
+import com.opensdk.framework.OpenSdk;
+
+import android.app.Application;
+
+public class MainApp extends Application {
+	public void onCreate()
+	  {
+	    System.out.println("ChinaMobileApplication :: onCreate");
+	    super.onCreate();
+	    System.out.println("start opensdk init" + System.currentTimeMillis());
+	    OpenSdk.getInstance().initApp(this);
+	    System.out.println("finish opensdk init" + System.currentTimeMillis());
+	  }
+}
